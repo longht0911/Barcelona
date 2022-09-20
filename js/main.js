c7a088a0992c08dda55600670079b6e0b29a67ff
longht0911 <hoanglonght0911@gmail.com>
2022-09-20 13:05:12 +0700

    upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Menu
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+const overlay = document.querySelector("#overlay");
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.toggle("navbar__open");
+  menuButton.classList.toggle("open");
+  overlay.classList.toggle("show");
+});
+
